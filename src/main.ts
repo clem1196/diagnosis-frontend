8206b2c0f4c9cf90f2ade79ba3bd8f047b27aa85
clem1196 <clementedela@hotmail.com>
2024-05-30 16:55:06 -0500

    fdfsdf

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,16 @@
-import './assets/main.css'
+import "./assets/main.css"
 
-import 'bootstrap'
+import "bootstrap"
 
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import '../node_modules/bootstrap-icons/font/bootstrap-icons.min.css'
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import "../node_modules/bootstrap-icons/font/bootstrap-icons.min.css"
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue"
+import App from "./App.vue"
+import router from "./router"
 
 const app = createApp(App)
 
 app.use(router)
 
-app.mount('#app')
+app.mount("#app")
