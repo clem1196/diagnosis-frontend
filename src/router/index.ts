92f34e288c6b9a916e84d0d640c60dbae25b2ef2
clem1196 <clementedela@hotmail.com>
2024-06-18 15:24:57 -0500

    bnnn

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,8 @@
 
 
 
-import { createRouter, createWebHistory } from 'vue-router'
+
+import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router'
 
 
 const router = createRouter({
